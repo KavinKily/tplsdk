fdb526dd4bb763301ba12af99c520fcaa8ed9bef
liweihong <gzlwhong101@gmail.com>
2024-10-08 17:52:13 +0800

    first commit

---
+++ Example/Pods/Target Support Files/tplsdk/tplsdk-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_tplsdk : NSObject
+@end
+@implementation PodsDummy_tplsdk
+@end
---
+++ tplsdk/Classes/TestSwift.swift
@@ -0,0 +1,14 @@
+//
+//  TestSwift.swift
+//  tplsdk
+//
+//  Created by liweihong on 2024/10/8.
+//
+
+import UIKit
+
+public class TestSwift: NSObject {
+    public func test111() {
+        print("---111-------")
+    }
+}
